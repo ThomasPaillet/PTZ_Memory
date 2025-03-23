dcf67b5224e5f6da02d266fadc6472f5d376a529
Thomas Paillet <75587285+ThomasPaillet@users.noreply.github.com>
2025-03-23 20:45:52 +0100

    Create sw_p_08.h

---
+++ sw_p_08.h
@@ -0,0 +1,59 @@
+/*
+ * copyright (c) 2025 Thomas Paillet <thomas.paillet@net-c.fr>
+
+ * This file is part of PTZ-Memory.
+
+ * PTZ-Memory is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * PTZ-Memory is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with PTZ-Memory. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef __TALLY_H
+#define __TALLY_H
+
+
+#ifdef _WIN32
+	#include <winsock2.h>
+#elif defined (__linux)
+	#include <sys/socket.h>
+#endif
+
+#include "ptz.h"
+
+
+#define SW_P_08_TCP_PORT 8000
+
+
+extern struct sockaddr_in sw_p_08_address;
+
+extern gboolean sw_p_08_server_started;
+
+extern char tally_cameras_set;
+
+
+void show_matrix_window (void);
+
+void tell_cameras_set_is_selected (gint page_num);
+
+void ask_to_connect_pgm_to_ctrl_opv (void);
+
+void ask_to_connect_ptz_to_ctrl_opv (ptz_t *ptz);
+
+void init_sw_p_08 (void);
+
+void start_sw_p_08 (void);
+
+void stop_sw_p_08 (void);
+
+
+#endif
+
