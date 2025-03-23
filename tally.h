363673345fa7df0c227f9a1e5464478c45ee4a6b
Thomas Paillet <75587285+ThomasPaillet@users.noreply.github.com>
2025-03-23 20:42:38 +0100

    Create tally.h

---
+++ tally.h
@@ -0,0 +1,55 @@
+/*
+ * copyright (c) 2025 Thomas Paillet <thomas.paillet@net-c.fr>
+
+ * This file is part of PTZ-Memory.
+
+ * PTZ-Memory is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * PTZ-Memory is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with PTZ-Memory. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef __TALLY_H
+#define __TALLY_H
+
+
+#include "ptz.h"
+
+
+#define TSL_UMD_V5_UDP_PORT 8900
+
+
+extern gboolean send_ip_tally;
+
+extern struct sockaddr_in tsl_umd_v5_address;
+
+
+gboolean tally_draw (GtkWidget *widget, cairo_t *cr, ptz_t *ptz);
+
+gboolean ghost_tally_draw (GtkWidget *widget, cairo_t *cr, ptz_t *ptz);
+
+gboolean control_window_tally_draw (GtkWidget *widget, cairo_t *cr, ptz_t *ptz);
+
+gboolean name_draw (GtkWidget *widget, cairo_t *cr, ptz_t *ptz);
+
+gboolean ghost_name_draw (GtkWidget *widget, cairo_t *cr, ptz_t *ptz);
+
+gboolean control_window_name_draw (GtkWidget *widget, cairo_t *cr, ptz_t *ptz);
+
+void init_tally (void);
+
+void start_tally (void);
+
+void stop_tally (void);
+
+
+#endif
+
