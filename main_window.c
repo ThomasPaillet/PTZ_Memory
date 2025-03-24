88ac030a6e91c5b919d42b31159fed7318833a1d
Thomas Paillet <75587285+ThomasPaillet@users.noreply.github.com>
2025-03-24 23:32:07 +0100

    Update main_window.c

--- main_window.c
+++ main_window.c
@@ -183,6 +183,8 @@ void main_window_notebook_switch_page (GtkNotebook *notebook, GtkWidget *page, g
 
 	g_mutex_unlock (&cameras_sets_mutex);
 
+	thumbnail_size = current_cameras_set->thumbnail_size;
+
 	if (page_num != tally_cameras_set) tell_cameras_set_is_selected (page_num);
 }
 
