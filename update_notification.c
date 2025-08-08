c35186708b1c39b222ce0cfa66b907fe94058a45
Thomas Paillet <75587285+ThomasPaillet@users.noreply.github.com>
2025-08-08 16:56:34 +0200

    Update update_notification.c

--- update_notification.c
+++ update_notification.c
@@ -22,6 +22,8 @@
 #include "cameras_set.h"
 #include "control_window.h"
 #include "error.h"
+#include "free_d.h"
+#include "logging.h"
 #include "protocol.h"
 
 #include <unistd.h>
@@ -50,6 +52,10 @@ gpointer receive_update_notification (gpointer data)
 	int i;
 	ptz_t *ptz, *other_ptz;
 	int response;
+	guint32 zoom;
+	guint32 focus;
+	gint32 pan;
+	gint32 tilt;
 
 	while (update_notification_started) {
 		addrlen = sizeof (struct sockaddr_in);
@@ -57,8 +63,14 @@ gpointer receive_update_notification (gpointer data)
 
 		if (src_socket == INVALID_SOCKET) break;
 
-		recv (src_socket, buffer, 556, 0);
+		recv (src_socket, buffer, sizeof (buffer), 0);
 		closesocket (src_socket);
+
+		if (logging && log_panasonic) {
+			g_mutex_lock (&logging_mutex);
+			log_ptz_update_notification (__FILE__, inet_ntoa (src_addr.sin_addr), buffer);
+			g_mutex_unlock (&logging_mutex);
+		}
 
 		if (buffer[30] == 'p') {
 			if (buffer[31] == '0') {		//P0	//Power Standby
@@ -86,6 +98,8 @@ gpointer receive_update_notification (gpointer data)
 							if (ptz == NULL) {
 								ptz = cameras_set_itr->cameras[i];
 
+								check_ptz_model (ptz);
+
 								send_cam_request_command (ptz, "QAF", &response);
 								if (response == 1) ptz->auto_focus = TRUE;
 								else ptz->auto_focus = FALSE;
@@ -93,27 +107,35 @@ gpointer receive_update_notification (gpointer data)
 								g_mutex_lock (&ptz->lens_information_mutex);
 
 								send_ptz_request_command_string (ptz, "#LPI", buffer);
-								if (buffer[3] <= '9') ptz->zoom_position = (buffer[3] - '0') * 256;
-								else ptz->zoom_position = (buffer[3] - '7') * 256;
-								if (buffer[4] <= '9') ptz->zoom_position += (buffer[4] - '0') * 16;
-								else ptz->zoom_position += (buffer[4] - '7') * 16;
-								if (buffer[5] <= '9') ptz->zoom_position += buffer[5] - '0';
-								else ptz->zoom_position += buffer[5] - '7';
 
 								ptz->zoom_position_cmd[4] = buffer[3];
 								ptz->zoom_position_cmd[5] = buffer[4];
 								ptz->zoom_position_cmd[6] = buffer[5];
 
-								if (buffer[6] <= '9') ptz->focus_position = (buffer[6] - '0') * 256;
-								else ptz->focus_position = (buffer[6] - '7') * 256;
-								if (buffer[7] <= '9') ptz->focus_position += (buffer[7] - '0') * 16;
-								else ptz->focus_position += (buffer[7] - '7') * 16;
-								if (buffer[8] <= '9') ptz->focus_position += buffer[8] - '0';
-								else ptz->focus_position += buffer[8] - '7';
+								if (buffer[3] <= '9') zoom = (buffer[3] - '0') * 256;
+								else zoom = (buffer[3] - '7') * 256;
+								if (buffer[4] <= '9') zoom += (buffer[4] - '0') * 16;
+								else zoom += (buffer[4] - '7') * 16;
+								if (buffer[5] <= '9') zoom += buffer[5] - '0';
+								else zoom += buffer[5] - '7';
 
 								ptz->focus_position_cmd[4] = buffer[6];
 								ptz->focus_position_cmd[5] = buffer[7];
 								ptz->focus_position_cmd[6] = buffer[8];
+
+								if (buffer[6] <= '9') focus = (buffer[6] - '0') * 256;
+								else focus = (buffer[6] - '7') * 256;
+								if (buffer[7] <= '9') focus += (buffer[7] - '0') * 16;
+								else focus += (buffer[7] - '7') * 16;
+								if (buffer[8] <= '9') focus += buffer[8] - '0';
+								else focus += buffer[8] - '7';
+
+								g_mutex_lock (&ptz->free_d_mutex);
+
+								ptz->zoom_position = zoom;
+								ptz->focus_position = focus;
+
+								g_mutex_unlock (&ptz->free_d_mutex);
 
 								send_ptz_control_command (ptz, "#LPC1", TRUE);
 
@@ -125,15 +147,20 @@ gpointer receive_update_notification (gpointer data)
 
 								g_mutex_lock (&other_ptz->lens_information_mutex);
 
-								other_ptz->zoom_position = ptz->zoom_position;
 								other_ptz->zoom_position_cmd[4] = buffer[3];
 								other_ptz->zoom_position_cmd[5] = buffer[4];
 								other_ptz->zoom_position_cmd[6] = buffer[5];
 
-								other_ptz->focus_position = ptz->focus_position;
 								other_ptz->focus_position_cmd[4] = buffer[6];
 								other_ptz->focus_position_cmd[5] = buffer[7];
 								other_ptz->focus_position_cmd[6] = buffer[8];
+
+								g_mutex_lock (&other_ptz->free_d_mutex);
+
+								other_ptz->zoom_position = zoom;
+								other_ptz->focus_position = focus;
+
+								g_mutex_unlock (&other_ptz->free_d_mutex);
 
 								g_mutex_unlock (&other_ptz->lens_information_mutex);
 
@@ -163,31 +190,43 @@ gpointer receive_update_notification (gpointer data)
 							g_mutex_lock (&ptz->lens_information_mutex);
 
 							if ((ptz->zoom_position_cmd[4] != buffer[33]) || (ptz->zoom_position_cmd[5] != buffer[34]) || (ptz->zoom_position_cmd[6] != buffer[35])) {
-								if (buffer[33] <= '9') ptz->zoom_position = (buffer[33] - '0') * 256;
-								else ptz->zoom_position = (buffer[33] - '7') * 256;
-								if (buffer[34] <= '9') ptz->zoom_position += (buffer[34] - '0') * 16;
-								else ptz->zoom_position += (buffer[34] - '7') * 16;
-								if (buffer[35] <= '9') ptz->zoom_position += buffer[35] - '0';
-								else ptz->zoom_position += buffer[35] - '7';
-
 								ptz->zoom_position_cmd[4] = buffer[33];
 								ptz->zoom_position_cmd[5] = buffer[34];
 								ptz->zoom_position_cmd[6] = buffer[35];
+
+								if (buffer[33] <= '9') zoom = (buffer[33] - '0') * 256;
+								else zoom = (buffer[33] - '7') * 256;
+								if (buffer[34] <= '9') zoom += (buffer[34] - '0') * 16;
+								else zoom += (buffer[34] - '7') * 16;
+								if (buffer[35] <= '9') zoom += buffer[35] - '0';
+								else zoom += buffer[35] - '7';
+
+								g_mutex_lock (&ptz->free_d_mutex);
+
+								ptz->zoom_position = zoom;
+
+								g_mutex_unlock (&ptz->free_d_mutex);
 
 								if (ptz == current_ptz) gtk_widget_queue_draw (control_window_zoom_level_bar_drawing_area);
 							}
 
 							if ((ptz->focus_position_cmd[4] != buffer[36]) || (ptz->focus_position_cmd[5] != buffer[37]) || (ptz->focus_position_cmd[6] != buffer[38])) {
-								if (buffer[36] <= '9') ptz->focus_position = (buffer[36] - '0') * 256;
-								else ptz->focus_position = (buffer[36] - '7') * 256;
-								if (buffer[37] <= '9') ptz->focus_position += (buffer[37] - '0') * 16;
-								else ptz->focus_position += (buffer[37] - '7') * 16;
-								if (buffer[38] <= '9') ptz->focus_position += buffer[38] - '0';
-								else ptz->focus_position += buffer[38] - '7';
-
 								ptz->focus_position_cmd[4] = buffer[36];
 								ptz->focus_position_cmd[5] = buffer[37];
 								ptz->focus_position_cmd[6] = buffer[38];
+
+								if (buffer[36] <= '9') focus = (buffer[36] - '0') * 256;
+								else focus = (buffer[36] - '7') * 256;
+								if (buffer[37] <= '9') focus += (buffer[37] - '0') * 16;
+								else focus += (buffer[37] - '7') * 16;
+								if (buffer[38] <= '9') focus += buffer[38] - '0';
+								else focus += buffer[38] - '7';
+
+								g_mutex_lock (&ptz->free_d_mutex);
+
+								ptz->focus_position = focus;
+
+								g_mutex_unlock (&ptz->free_d_mutex);
 
 								if (ptz == current_ptz) gtk_widget_queue_draw (control_window_focus_level_bar_drawing_area);
 							}
@@ -196,20 +235,32 @@ gpointer receive_update_notification (gpointer data)
 
 							g_mutex_lock (&other_ptz->lens_information_mutex);
 
-							if (other_ptz->zoom_position != ptz->zoom_position) {
-								other_ptz->zoom_position = ptz->zoom_position;
+							if (other_ptz->zoom_position != zoom) {
+								other_ptz->zoom_position = zoom;
 								other_ptz->zoom_position_cmd[4] = buffer[33];
 								other_ptz->zoom_position_cmd[5] = buffer[34];
 								other_ptz->zoom_position_cmd[6] = buffer[35];
 
+								g_mutex_lock (&other_ptz->free_d_mutex);
+
+								other_ptz->zoom_position = zoom;
+
+								g_mutex_unlock (&other_ptz->free_d_mutex);
+
 								if (other_ptz == current_ptz) gtk_widget_queue_draw (control_window_zoom_level_bar_drawing_area);
 							}
 
-							if (other_ptz->focus_position != ptz->focus_position) {
-								other_ptz->focus_position = ptz->focus_position;
+							if (other_ptz->focus_position != focus) {
+								other_ptz->focus_position = focus;
 								other_ptz->focus_position_cmd[4] = buffer[36];
 								other_ptz->focus_position_cmd[5] = buffer[37];
 								other_ptz->focus_position_cmd[6] = buffer[38];
+
+								g_mutex_lock (&other_ptz->free_d_mutex);
+
+								other_ptz->focus_position = focus;
+
+								g_mutex_unlock (&other_ptz->free_d_mutex);
 
 								if (other_ptz == current_ptz) gtk_widget_queue_draw (control_window_focus_level_bar_drawing_area);
 							}
@@ -223,6 +274,62 @@ gpointer receive_update_notification (gpointer data)
 			}
 
 			if (ptz != NULL) g_mutex_unlock (&ptz->lens_information_mutex);
+
+			g_mutex_unlock (&cameras_sets_mutex);
+		} else if ((buffer[30] == 'a') && (buffer[31] == 'P') && (buffer[32] == 'C')) { 	//aPC	//Pan/tilt position
+			ptz = NULL;
+
+			g_mutex_lock (&cameras_sets_mutex);
+
+			for (cameras_set_itr = cameras_sets; cameras_set_itr != NULL; cameras_set_itr = cameras_set_itr->next) {
+				for (i = 0; i < cameras_set_itr->number_of_cameras; i++) {
+					if (cameras_set_itr->cameras[i]->address.sin_addr.s_addr == src_addr.sin_addr.s_addr) {
+						if (ptz == NULL) {
+							ptz = cameras_set_itr->cameras[i];
+
+							if (buffer[33] <= '9') pan = (buffer[33] - '0') * 4096;
+							else pan = (buffer[33] - '7') * 4096;
+							if (buffer[34] <= '9') pan += (buffer[34] - '0') * 256;
+							else pan += (buffer[34] - '7') * 256;
+							if (buffer[35] <= '9') pan += (buffer[35] - '0') * 16;
+							else pan += (buffer[35] - '7') * 16;
+							if (buffer[36] <= '9') pan += buffer[36] - '0';
+							else pan += buffer[36] - '7';
+
+							if (buffer[37] <= '9') tilt = (buffer[37] - '0') * 4096;
+							else tilt = (buffer[37] - '7') * 256;
+							if (buffer[38] <= '9') tilt += (buffer[38] - '0') * 256;
+							else tilt += (buffer[38] - '7') * 256;
+							if (buffer[39] <= '9') tilt += (buffer[39] - '0') * 16;
+							else tilt += (buffer[39] - '7') * 16;
+							if (buffer[40] <= '9') tilt += buffer[40] - '0';
+							else tilt += buffer[40] - '7';
+
+							g_mutex_lock (&ptz->free_d_mutex);
+
+							ptz->free_d_Pan = (0x8000 - pan) * 270;
+							ptz->free_d_Tilt = (0x8000 - tilt) * 270;
+
+							g_mutex_unlock (&ptz->free_d_mutex);
+
+							if (ptz == current_ptz) gtk_widget_queue_draw (control_window_pan_tilt_label);
+						} else {
+							other_ptz = cameras_set_itr->cameras[i];
+
+							g_mutex_lock (&other_ptz->free_d_mutex);
+
+							other_ptz->free_d_Pan = (0x8000 - pan) * 270;
+							other_ptz->free_d_Tilt = (0x8000 - tilt) * 270;
+
+							g_mutex_unlock (&other_ptz->free_d_mutex);
+
+							if (other_ptz == current_ptz) gtk_widget_queue_draw (control_window_pan_tilt_label);
+						}
+
+						break;
+					}
+				}
+			}
 
 			g_mutex_unlock (&cameras_sets_mutex);
 		} else if ((buffer[30] == 'O') && (buffer[31] == 'A') && (buffer[32] == 'F')) { 	//OAF:	//Auto focus_position
